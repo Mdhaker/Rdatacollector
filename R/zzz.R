571cc193601f1a3a3ffdbf8fb0ed50ec0d2b486e
Mdhaker <med.dhaker.abdeljawed@gmail.com>
2017-07-13 15:51:55 +0200

    checking package dependencies

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,18 @@
 .onLoad <- function(libname, pkgname)
 {
+  #installing rJava
+  if(! ("rJava" %in% rownames(installed.packages())) )
+    {
+      install.packages("rJava");
+    }
+  #installing stringi
+  if(! ("stringi" %in% rownames(installed.packages())) )
+    {
+      install.packages("stringi");
+    }
+  if( stringi::stri_length(Sys.getenv("JAVA_HOME")) > 0)
+    {
+    
   rJava::.jpackage(pkgname, lib.loc=libname);
   rJava::.jinit();
   rJava::.jaddClassPath(paste0(find.package(pkgname),"/java/JScrap.jar"));
@@ -7,6 +20,9 @@
   scrapInterface <- rJava::.jnew("utils/Rinterface",url);
   socialInterface <<- rJava::.jnew("com/datacollection/utils/Rinterface");
   rJava::.jcall(scrapInterface,"V","setChromeDriverPath",getwd());
+    }
+  else
+    print("please setup java envirement");
 }
 #' @export
 enableDebug <- function(debug)
